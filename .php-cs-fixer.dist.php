61beada7cd39c118c8f35a9db29bdd3be030bdc7
Ruslan Ora <ruslan.ora@outlook.com>
2026-07-11 22:17:53 -0400

    chore: define a project scaffold
    - define a folder structure
    - create and extend `composer.json`
    - add and configure PHPUnit for testing
    - add and configure PHP CS Fixer for formatting
    - add `.editorconfig`
    - add `.gitattributes`, and extend `.gitignore`
    - update `LICENSE` and `README.md`

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,18 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in([__DIR__ . '/src', __DIR__ . '/tests'])
+    ->name('*.php');
+
+return (new PhpCsFixer\Config())
+    ->setRiskyAllowed(true)
+    ->setRules([
+        '@PSR12' => true,
+        '@PSR12:risky' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'declare_strict_types' => true,
+        'no_unused_imports' => true,
+        'ordered_imports' => true,
+        'trailing_comma_in_multiline' => true,
+    ])
+    ->setFinder($finder);
